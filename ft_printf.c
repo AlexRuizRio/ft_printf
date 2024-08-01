ad01a34f83c6f2ed6779af1f282e2e6772fd3dd8
AlexRuizRio <alejandro.ruizrio@gmail.com>
2024-08-01 22:42:32 +0200

    Estudio y empiece del metodo

--- ft_printf.c
+++ ft_printf.c
@@ -1,7 +1,19 @@
-
 #include "ft_printf.h"
 
-int	ft_printf(char const *format, ...)
+int	ft_printf(const char *format, ...)
 {
+    va_list args;
+    int nbytes;
+    
+    va_start(args, format);
+    
+    return (nbytes);
+}
+
+int main()
+{
+    ft_putchar_fd('a', 1);
+    ft_putchar_fd('\n', 1);
+    printf("Estoy probando %d asi que %d esperar", 10, 30);
     return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -1,12 +1,14 @@
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include <stdio.h>
 # include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
+#include <stdarg.h> 
+# include "libft/libft.h"
 
-int ft_printf(char const *format, ...);
+int ft_printf(const char *format, ...);
 
 #endif
