8926268e1243e93317e1e2204f5268d4708baf0d
AlexRuizRio <alejandro.ruizrio@gmail.com>
2024-08-04 12:43:09 +0200

    Logica de los posibles formatos

--- ft_printf.c
+++ ft_printf.c
@@ -1,19 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alruiz-d <alruiz-d@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 11:24:51 by alruiz-d          #+#    #+#             */
+/*   Updated: 2024/08/04 12:40:08 by alruiz-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-int	ft_printf(const char *format, ...)
+static int conversions (const char *format, va_list args);
+
+int ft_printf(const char *format, ...)
 {
     va_list args;
+    const char *ptr = format;
     int nbytes;
-    
+
     va_start(args, format);
-    
+     while (*ptr)
+    {
+        if (*ptr == '%' && *(ptr + 1))
+        {
+            ptr++;
+            nbytes += conversions (ptr, args);
+        }
+        else
+        {
+            ft_putchar_fd(*ptr, 1); // tiene que devolver un int
+        }
+        ptr++;
+    }
     return (nbytes);
+    va_end(args);
+}
+
+
+static int conversions (const char ptr, va_list args)
+{
+    if(ptr == '%')
+        return(0);
+    else if (ptr == 'c')
+        return (0);
+    else if (ptr == 's')
+        return (0);
+    else if (ptr == 'p')
+        return (0);
+    else if (ptr == 'd' || ptr == 'i')
+        return (0);
+    else if (ptr == 'u')
+        return (0);
+    else if (ptr == 'x' || ptr == 'X')
+        return (0);
+    return (0);
 }
 
 int main()
 {
     ft_putchar_fd('a', 1);
     ft_putchar_fd('\n', 1);
-    printf("Estoy probando %d asi que %d esperar", 10, 30);
+    int printtf = ft_printf("Estoy probando %d asi que %d esperar \n", 10, 30);
+
+    int print = printf("Estoy probando %d asi que %d esperar \n", 10, 30);
+
+    printf("el numero en print es %d y el numero en printf es %d \n", print, printtf);
     return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alruiz-d <alruiz-d@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 11:24:36 by alruiz-d          #+#    #+#             */
+/*   Updated: 2024/08/03 11:24:37 by alruiz-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
@@ -6,7 +18,7 @@
 # include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
-#include <stdarg.h> 
+# include <stdarg.h> 
 # include "libft/libft.h"
 
 int ft_printf(const char *format, ...);
