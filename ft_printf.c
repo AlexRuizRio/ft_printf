9b63b445ba01ab989cac3fcfb1e84068cc5affb6
AlexRuizRio <alejandro.ruizrio@gmail.com>
2024-08-12 13:28:50 +0200

    Impresion de formatos %c y %s

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,16 @@
 /*   By: alruiz-d <alruiz-d@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 11:24:51 by alruiz-d          #+#    #+#             */
-/*   Updated: 2024/08/04 12:40:08 by alruiz-d         ###   ########.fr       */
+/*   Updated: 2024/08/12 13:26:40 by alruiz-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int conversions (const char *format, va_list args);
+static int conversions (const char ptr, va_list args);
+int ft_putchar (char c);
+int ft_strlens ( char *str);
+int	ft_putstr(char *s);
 
 int ft_printf(const char *format, ...)
 {
@@ -21,32 +24,33 @@ int ft_printf(const char *format, ...)
     int nbytes;
 
     va_start(args, format);
+    nbytes = 0;
      while (*ptr)
     {
         if (*ptr == '%' && *(ptr + 1))
         {
             ptr++;
-            nbytes += conversions (ptr, args);
+            nbytes += conversions (*ptr, args);
         }
         else
         {
-            ft_putchar_fd(*ptr, 1); // tiene que devolver un int
+            nbytes += ft_putchar(*ptr); // tiene que devolver un int
         }
         ptr++;
     }
-    return (nbytes);
     va_end(args);
-}
+    return (nbytes);
 
+}
 
 static int conversions (const char ptr, va_list args)
 {
     if(ptr == '%')
         return(0);
     else if (ptr == 'c')
-        return (0);
+        return (ft_putchar(va_arg(args, int)));
     else if (ptr == 's')
-        return (0);
+        return (ft_putstr(va_arg(args, char *)));
     else if (ptr == 'p')
         return (0);
     else if (ptr == 'd' || ptr == 'i')
@@ -57,15 +61,52 @@ static int conversions (const char ptr, va_list args)
         return (0);
     return (0);
 }
+int	ft_putstr(char *s)
+{
+	int	len;
 
+    len = 0;
+    if(s == NULL)
+    {
+        return (ft_putstr("(null)"));
+    }
+	len = ft_strlens(s);
+	write(1, s, len);
+    return (len);
+}
+
+int ft_putchar ( char c)
+{
+    return(write(1, &c, 1));
+}
+
+int ft_strlens ( char *str)
+{
+    int len;
+    
+    if(str == NULL)
+        return (0);
+    len = 0;
+    while(*str)
+    {
+        len += 1;
+        str++;
+    }
+     return (len);
+    
+}
 int main()
 {
-    ft_putchar_fd('a', 1);
-    ft_putchar_fd('\n', 1);
-    int printtf = ft_printf("Estoy probando %d asi que %d esperar \n", 10, 30);
+    ft_putchar('a');
+    ft_putchar('\n');
+    char *str = NULL;
+    int printtfi = ft_printf("Estoy probando %c asi que %c esperar \n", 'a', 'a');
+    int printi = printf("Estoy probando %c asi que %c esperar \n", 'a', 'a');
 
-    int print = printf("Estoy probando %d asi que %d esperar \n", 10, 30);
+    int printtf = ft_printf("Hola %s \n", str);
+    int print = printf("Hola %s \n", str);
 
     printf("el numero en print es %d y el numero en printf es %d \n", print, printtf);
+    printf("el numero en print es %d y el numero en printf es %d \n", printi, printtfi);
     return (0);
 }
